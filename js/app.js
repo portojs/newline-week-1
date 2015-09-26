1fd2c1dd30f0dbc39e8f876c29fd1588cd1e66c0
peter <peter.k.nets@gmail.com>
2015-09-26 19:44:13 +0300

    Add other pages, add some js code

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
 /**
  * Created by Peter on 26.09.2015.
  */
+function PageNavigation() {
+    this.showPage = function() {
+        if ($(this).hasClass("button_about_us")){
+
+        } else if ($(this).hasClass("button_to_the_moon")){
+
+        } else if ($(this).hasClass("button_contacts")){
+
+        }
+    };
+    $(".header").on("click.show_page", ".button", this.showPage);
+}
+
 $(document).ready(function(){
-    var pageNavigation = new pageNavigation();
+    var pageNavigation = new PageNavigation();
 });
