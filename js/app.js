92f8591957a04389c2fd5ca8080f45145d72089f
peter <peter.k.nets@gmail.com>
2015-09-26 20:00:51 +0300

    Try 1st column content change on button click

--- js/app.js
+++ js/app.js
@@ -4,11 +4,17 @@
 function PageNavigation() {
     this.showPage = function() {
         if ($(this).hasClass("button_about_us")){
-
+            $(this).closest(".nav").find("#to_the_moon_page").slideUp();
+            $(this).closest(".nav").find("#contacts_page").slideUp();
+            $(this).closest(".main_window").find("#about_us_page").slideDown();
         } else if ($(this).hasClass("button_to_the_moon")){
-
+            $(this).closest(".nav").find("#about_us_page").slideUp();
+            $(this).closest(".nav").find("#contacts_page").slideUp();
+            $(this).closest(".main_window").find("#to_the_moon_page").slideDown();
         } else if ($(this).hasClass("button_contacts")){
-
+            $(this).closest(".nav").find("#to_the_moon_page").slideUp();
+            $(this).closest(".nav").find("#about_us_page").slideUp();
+            $(this).closest(".main_window").find("#contacts_page").slideDown();
         }
     };
     $(".header").on("click.show_page", ".button", this.showPage);
