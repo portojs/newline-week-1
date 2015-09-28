48e3d1924ec9445dc4cedc2a4a25ecbb411a8309
peter <peter.k.nets@gmail.com>
2015-09-28 13:44:29 +0300

    Repair showPage function 2

--- js/app.js
+++ js/app.js
@@ -6,15 +6,15 @@ function PageNavigation() {
         if ($(this).hasClass("button_about_us")){
             $(this).closest(".main_window").find("#to_the_moon_page").hide();
             $(this).closest(".main_window").find("#contacts_page").hide();
-            $(this).closest(".main_window").find("#about_us_page").fadeIn();
+            $(this).closest(".main_window").find("#about_us_page").fadeIn("slow");
         } else if ($(this).hasClass("button_to_the_moon")){
             $(this).closest(".main_window").find("#about_us_page").hide();
             $(this).closest(".main_window").find("#contacts_page").hide();
-            $(this).closest(".main_window").find("#to_the_moon_page").fadeIn();
+            $(this).closest(".main_window").find("#to_the_moon_page").fadeIn("slow");
         } else if ($(this).hasClass("button_contacts")){
             $(this).closest(".main_window").find("#to_the_moon_page").hide();
             $(this).closest(".main_window").find("#about_us_page").hide();
-            $(this).closest(".main_window").find("#contacts_page").fadeIn();
+            $(this).closest(".main_window").find("#contacts_page").fadeIn("slow");
         }
     };
     $(".header").on("click.show_page", ".button", this.showPage);
